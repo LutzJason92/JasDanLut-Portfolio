e6dae742d9266640b82214c5f2073c59fff8a472
Jason <LutzJason92@gmail.com>
2023-08-10 11:41:16 -0600

    Update NavBar.jsx
    insert {Anikin "its working".gif}

--- jasdanlutapp/components/navBar/NavBar.jsx
+++ jasdanlutapp/components/navBar/NavBar.jsx
@@ -1,69 +1,27 @@
 "use client";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "react-bootstrap/Nav";
-// import styles from "./navBar.module.css";
-
-// const links = [
-//   {
-//     id: 1,
-//     title: "About",
-//     url: "/about",
-//   },
-//   {
-//     id: 2,
-//     title: "Connect",
-//     url: "/connect",
-//   },
-//   {
-//     id: 3,
-//     title: "Documents",
-//     url: "/documents",
-//   },
-//   {
-//     id: 4,
-//     title: "Portfolio",
-//     url: "/portfolio",
-//   },
-// ];
-
+import styles from "./navBar.module.css";
 const NavBar = () => {
   return (
-    <>
+    
+    
     <Nav className={styles.navBar} activeKey="/about">
       <Nav.Item>
         <Nav.Link href="/about">About</Nav.Link>
       </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/connect">Connect</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/documents">Documents</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+      </Nav.Item>
     </Nav>
-    </>
+    
   );
 };
 
-// export default NavBar;
-
-// <div className={styles.container}>
-//       <div className={styles.name}>
-//         <Link href="/">Jason Daniel Lutz</Link>
-//       </div>
-//       <div className={styles.tabCluster}>
-//         <TabPane>
-//         <div className={styles.tabButtons}>
-//           {links.map((link) => (
-//             <Tab
-//               key={link.id}
-//               href={link.url}
-              
-//             >
-//               {link.title}
-//             </Tab>
-//           ))}
-//         </div>
-//         </TabPane>
-//       </div>
-//       <button
-//       className={styles.loginOut}
-//         onClick={() => {
-//           console.log("logged out");
-//         }}
-//       >
-//         logout
-//       </button>
-//     </div>
+export default NavBar;
